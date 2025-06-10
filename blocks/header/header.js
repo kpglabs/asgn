5a47c49a03d76241462633fe36dfcd4b717365b4
kpglabs <kavyakeerthana94@gmail.com>
2025-06-10 19:32:59 +0530

    1

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -226,7 +226,7 @@ export default async function decorate(block) {
   hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
       <span class="nav-hamburger-icon"></span>
     </button>`;
-  hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
+  hamburger.addEventListener('mouseover', () => toggleMenu(nav, navSections));
   nav.prepend(hamburger);
   nav.setAttribute('aria-expanded', 'false');
   // prevent mobile nav behavior on window resize
