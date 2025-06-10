6906ba383435a20c6981fa91a378fc6a08e1fc19
kpglabs <kavyakeerthana94@gmail.com>
2025-06-10 19:52:53 +0530

    1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -67,7 +67,7 @@ function buildBreadcrumbBlock(main) {
   if (window.location.pathname !== '/' && window.isErrorPage !== true && !getMetadata('hideBreadcrumb')) {
     const section = createElement('div');
     section.append(buildBlock('breadcrumb', { elems: [] }));
-    main.prepend(section);
+    main.append(section);
   }
 }
 /**
