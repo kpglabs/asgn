6e9f52d7082877115e10d1970b8ed51761966021
kpglabs <kavyakeerthana94@gmail.com>
2025-06-11 11:58:35 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -199,10 +199,19 @@ export default async function decorate(block) {
   }
 
   const navSections = nav.querySelector('.nav-sections');
+  
   if (navSections) {
     navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
-      if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+      if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');    
       navSection.addEventListener('mouseover', () => {
+        if (isDesktop.matches) {
+          const expanded = navSection.getAttribute('aria-expanded') === 'true';
+          toggleAllNavSections(navSections);
+          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        }
+      });
+    
+        navSection.ul.addEventListener('mouseover', () => {
         if (isDesktop.matches) {
           const expanded = navSection.getAttribute('aria-expanded') === 'true';
           toggleAllNavSections(navSections);
